620198279076c03b9253af93ac2873a422577c1a
Sakshi Dinesh Dhandar <139968907+sakshidhandar9@users.noreply.github.com>
2025-01-22 10:46:07 +0530

    Add files via upload

---
+++ Restorant Website/script.js
@@ -0,0 +1,10 @@
+// script.js
+// JavaScript (script.js)
+
+
+document.querySelector('.hide-button').addEventListener('click', function() {
+    document.querySelector('.container4').classList.toggle('hide');
+    this.classList.toggle('invisible'); // Toggle 'invisible' class on the button itself
+});
+    
+
